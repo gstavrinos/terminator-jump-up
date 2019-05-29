08ef0c13a61268d7a5b069d418a53f699971ca45
George Stavrinos <gstavrinos@iit.demokritos.gr>
2019-05-29 16:49:14 +0300

    Frist stable version

--- jump.py
+++ jump.py
@@ -27,21 +27,35 @@ class JumpUp(plugin.MenuItem):
 
     def jumpUp(self, widget):
         try:
-            #t = Terminator().get_focussed_terminal().get_vte()
-            t = Terminator().terminals[0].get_vte()
-            dbg('\033[1;31mJumpUp row: %s\033[0m' % t.scrollbar_position())
-            col, row = t.get_cursor_position()
-            content = t.get_text_range(0, 0, row, col, lambda *a: True).split("\n")[:-1]
-            dbg('\033[1;31mJumpUp content: %s\033[0m' % content)
-            dbg('\033[1;31mJumpUp col: %s\033[0m' % col)
-            dbg('\033[1;31mJumpUp row: %s\033[0m' % row)
-            #t.set_cursor_position(col, 0)
-            #https://stackoverflow.com/questions/11353184/gtk-programmatically-scroll-back-a-single-line-in-scrolled-window-containing-a
+            t = Terminator().last_focused_term
+            col, row = t.get_vte().get_cursor_position()
+            content = t.get_vte().get_text_range(0, 0, row, col, lambda *a: True).split("\n")
+            width = t.get_vte().get_column_count()
+            last_username = content[-2].split("@")[0]
+            last_hostname = content[-2].split("@")[1].split(":")[0]
+            content_as_seen = []
+            for line in content[:-2]:
+                if len(line) > width:
+                    new_lines = [line[i:i+width] for i in range(0, len(line), width)]
+                    for n in new_lines:
+                        content_as_seen.append(n)
+                else:
+                    content_as_seen.append(line)
+            machine_username = getuser()
+            machine_hostname = gethostname()
+            jumpPos = 0
+            # Work even when we are connected on another machine, with different username/hostname
+            ssh_check = machine_username != last_username and machine_hostname != last_hostname
+            ssh_prefix = last_username + "@" + last_hostname + ":"
+            normal_prefix = machine_username + "@" + machine_hostname + ":"
+            for i in range(len(content_as_seen)-1, 0, -1):
+                if (not (content_as_seen[i].endswith("$ ^C") or content_as_seen[i].endswith("$ ")) and (content_as_seen[i].startswith(normal_prefix) or (ssh_check and content_as_seen[i].startswith(ssh_prefix)))):
+                    jumpPos = i
+                    break
+            t.scrollbar_jump(jumpPos)
         except Exception, ex:
             dbg('\033[1;31mJumpUp failed: %s\033[0m' % ex)
 
     def onKeyPress(self, widget, event):
-        dbg('\033[1;31mJumpUp user: %s\033[0m' % getuser())
-        dbg('\033[1;31mJumpUp hostname: %s\033[0m' % gethostname())
         if (event.state & gtk.gdk.MOD1_MASK == gtk.gdk.MOD1_MASK) and (event.keyval == 74 or event.keyval == 106): # Alt+J or Alt+j
             self.jumpUp(widget)
